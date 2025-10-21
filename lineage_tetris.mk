d669f0e670fed75486f14db8a26be934c842ffc4
abdulla-lin <abdullashakkeeb4@gmail.com>
2025-10-21 16:08:30 +0530

    Update lineage_tetris.mk

--- lineage_tetris.mk
+++ lineage_tetris.mk
@@ -17,9 +17,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 persist.vendor.power.adpf.enable=true \
 ro.vendor.powerhal.adpf.enable=true
 
-#GMS FLAG
-WITH_GMS=true
-
 # Inherit from tetris device
 $(call inherit-product, device/nothing/tetris/device.mk)
 
