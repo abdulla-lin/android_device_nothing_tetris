cf3c5e80228e025db46f03b8399334070c24ece6
abdulla-lin <abdullashakkeeb4@gmail.com>
2025-10-22 00:03:33 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,3 +12,5 @@ fi
 if [ ! -d "device/nothing/tetris-kernel" ]; then
   git clone https://github.com/SuperAviation001/android_device_nothing_tetris-kernel -b lineage-22.2 device/nothing/tetris-kernel
 fi
+if [ ! -d "packages/apps/ViPER4AndroidFX" ]; then
+  git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX -b v4a packages/apps/ViPER4AndroidFX
