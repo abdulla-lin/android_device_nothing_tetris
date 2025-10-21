0c5f4878f77988dd3c83e0a2f2067047bb7bed16
abdulla-lin <abdullashakkeeb4@gmail.com>
2025-10-21 23:57:15 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -23,6 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 # Setup dalvik vm configs
 $(call inherit-product,frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+#v4a setup
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
