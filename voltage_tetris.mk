4309dbea0009de211ad7009a70e40b2fcfea433d
abdulla-lin <abdullashakkeeb4@gmail.com>
2025-10-23 19:42:34 +0530

    Update voltage_tetris.mk

--- voltage_tetris.mk
+++ voltage_tetris.mk
@@ -20,6 +20,21 @@ ro.vendor.powerhal.adpf.enable=true
 #gms
 WITH_GMS := false
 
++# VoltageOS Flags
++EXTRA_UDFPS_ANIMATIONS := true
++PERF_ANIM_OVERRIDE := true
++
++# VoltageOS CPUSets
++VOLTAGE_CPU_SMALL_CORES := 0,1,2,3
++VOLTAGE_CPU_BIG_CORES := 4,5,6,7
++
++VOLTAGE_ALL_CORES := 0-7
++VOLTAGE_CPU_BG := 0-3
++VOLTAGE_CPU_FG := 0-6
++VOLTAGE_CPU_LIMIT_BG := 0-1
++VOLTAGE_CPU_LIMIT_UI := 4-7
++VOLTAGE_CPU_DISPLAY := 4-7
++
 # Inherit from tetris device
 $(call inherit-product, device/nothing/tetris/device.mk)
 
